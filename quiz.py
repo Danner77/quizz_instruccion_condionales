49ed3cd2b225a57a957d419355427d0dd16793e5
Danner77 <danielvillar@colegioguanenta.edu.co>
2025-02-13 16:20:18 -0500

    menaje

--- quiz.py
+++ quiz.py
@@ -1,13 +1,1 @@
-# Quiz instrucciones condicionales
-"""Dados tres números a, b y c, correspondientes a la longitud de los lados de una figura geométrica, determinar si pueden formar los lados 
 
-
-print("------------------------------")
-print("----- VERIFICAR TRIANGULO ----")
-print("------------------------------")
-
-# input
-
-# processing
-
-# output
